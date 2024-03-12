6b38cceb040da623649d21c2f2bf332fce2d0274
dmytro <dmytro.cherepov.ukr@gmail.com>
2024-03-12 10:32:09 +0200

    added solution

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Enter your city..")
+    val city = readlnOrNull()
+    println("User lives in $city")
+
+}
